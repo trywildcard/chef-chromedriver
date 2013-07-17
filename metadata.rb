3b103ce4da21a81e8e82df2a93ac2946bbe9d9cd
strcrzy <bramiejim@gmail.com>
2013-07-17 17:03:46 -0400

    Update metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'rallysoftware-cookbooks@rallydev.com'
 license          'MIT'
 description      'Installs/Configures chromedriver'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.4'
+version          '0.1.5'
 
 recipe 'chromedriver::default', 'Installs chromedriver using ark'
 
