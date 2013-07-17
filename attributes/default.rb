e38401274e40ec336fe6a34de3acb442983ca954
strcrzy <bramiejim@gmail.com>
2013-07-17 16:16:36 -0400

    dont ark install into /usr/bin or it will clobber permissions

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,7 +1,7 @@
 default['chromedriver']['version'] = 'linux64_26.0.1383.0'
 default['chromedriver']['archive_name']  = "chromedriver_#{node['chromedriver']['version']}.zip"
 default['chromedriver']['source_url']  = "https://chromedriver.googlecode.com/files/#{node['chromedriver']['archive_name']}"
-default['chromedriver']['path'] = '/usr/bin'
+default['chromedriver']['path'] = '/opt/chromedriver'
 default['chromedriver']['owner'] = 'root'
 default['chromedriver']['group'] = 'root'
 default['chromedriver']['mode'] = 755
