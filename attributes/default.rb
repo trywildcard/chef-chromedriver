56dc7dd48db64264b711ecb0fb6317c13293ce47
strcrzy <bramiejim@gmail.com>
2013-07-17 19:34:40 -0400

    update attributes
    remove chromedriver.path attribute, as it is not used by :install action
    set the mode attribute to a string

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,7 +1,6 @@
 default['chromedriver']['version'] = 'linux64_2.1'
 default['chromedriver']['archive_name']  = "chromedriver_#{node['chromedriver']['version']}.zip"
 default['chromedriver']['source_url']  = "https://chromedriver.googlecode.com/files/#{node['chromedriver']['archive_name']}"
-default['chromedriver']['path'] = '/opt/chromedriver'
 default['chromedriver']['owner'] = 'root'
 default['chromedriver']['group'] = 'root'
-default['chromedriver']['mode'] = 755
+default['chromedriver']['mode'] = '755'
