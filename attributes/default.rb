360f7a1e61c58f1c2508fbe5c0b580dd58f4d934
Eric Tang <eric.x.tang@gmail.com>
2014-04-23 16:04:01 -0400

    Update default.rb

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,4 +1,4 @@
-default['chromedriver']['version'] = 'linux64_2.1'
+default['chromedriver']['version'] = 'linux64_2.9'
 default['chromedriver']['archive_name']  = "chromedriver_#{node['chromedriver']['version']}.zip"
 default['chromedriver']['source_url']  = "https://chromedriver.googlecode.com/files/#{node['chromedriver']['archive_name']}"
 default['chromedriver']['path'] = '/opt/chromedriver'
